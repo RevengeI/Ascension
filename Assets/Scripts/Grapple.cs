fb3c7f1822ed5e6b200f270ff6454d5787272ccf
WillowPunch <danek2002@gmail.com>
2023-04-09 15:52:12 +0300

    UPM Additional

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -9,120 +9,59 @@ public class Grapple : MonoBehaviour
     public float speed = 20;
     public float height = 0.18f;
     public float width = 1f;
+    public float timeToLive = 0;
     public LayerMask Ground;
-    private bool Attached = false;
-    private float radius = 0.5f;
-    private float degrees = 1.5f;
-    private double angle;
-    private float originX;
-    private float originY;
-    public Rigidbody2D rigidGravity;
-    private int accel = 30000000;
-    private int resistance = 0;
-    public enum ButtonPress
-    {
-        Right,
-        Left,
-        Null
-    }
-    public ButtonPress Orientation;
-
-
+    public DistanceJoint2D joint;
+    private bool notAttached = true;
+    private Rigidbody2D rigid;
+    //private Rigidbody2D gravityPlayer;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        rigidGravity = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         transform.position = player.transform.position;
-        angle = Math.PI * degrees;
-        Orientation = ButtonPress.Null;
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        //gravityPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        if (Attached)
+        if (notAttached)
         {
-            Rotation();
-        }    
-        else
-        { 
             transform.Translate((speed * Time.deltaTime), (speed * Time.deltaTime), 0);
-            if (Physics2D.OverlapBox(transform.position, new Vector2(width, height), 0, Ground))
-            {
-                player.transform.position = transform.position + new Vector3(0, (-1)*radius, 0);
-                originX = player.transform.position.x;
-                originY = player.transform.position.y;
-                rigidGravity.gravityScale = 0f;
-                Attached = true;
-            }
-            if (Input.GetKeyUp(KeyCode.X))
-            {
-                Destroy(gameObject);
-            }
         }
-    }
+        if (Physics2D.OverlapBox(transform.position, new Vector2(width, height), 0, Ground))
+        {
+            if (notAttached) // do not delete. to do: fix joint behaviour, works badly
+            {
+                
+                notAttached = false;
+                rigid.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                joint = gameObject.AddComponent(typeof(DistanceJoint2D)) as DistanceJoint2D;
+                joint.connectedBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+                joint.anchor = transform.position;
+                joint.autoConfigureDistance = false;
+                joint.distance = 1f;
+                
+            }
+           /* if(!notAttached)
+            {
 
-    void Rotation()
-    {
+                
+                timeToLive += Time.deltaTime;
+                if(timeToLive > 1f)
+                {
+                    // gravityPlayer.AddForce(joint.reactionForce, ForceMode2D.Impulse);
+                    Destroy(gameObject);
+                }
+            }*/
+        }
         if (Input.GetKeyUp(KeyCode.X))
         {
+            //if(!notAttached)
+            //{ gravityPlayer.AddForce(joint.reactionForce, ForceMode2D.Impulse); }
+
             Destroy(gameObject);
-            Attached = false;
-            rigidGravity.gravityScale = 4f;
-        }
-        else
-        {
-            DeclareOrientation();
-            if (Orientation == ButtonPress.Right)
-            {
-                accel += 2000;
-            }
-            if (Orientation == ButtonPress.Left)
-            {
-                accel -= 2000;
-            }
-            if (Orientation == ButtonPress.Null)
-            {
-                while (accel != 30000000)
-                {
-                    if (accel > 30000000)
-                    {
-                        accel -= 1;
-                    }
-                    else
-                    {
-                        accel += 1;
-                    }    
-                }
-            }
-            Debug.Log(accel);
-            degrees = 0.00000005f * (float)accel;
-            angle = Math.PI * degrees; //default 1.5f
-            player.transform.position = new Vector2(originX + (float)Math.Cos(angle) * radius, originY + (float)Math.Sin(angle) * radius);
         }
     }
 
-    void DeclareOrientation()
-    {
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
-        {
-            Orientation = ButtonPress.Null;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Orientation = ButtonPress.Right;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Orientation = ButtonPress.Left;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            radius = 3f;
-        }
-    }
+    
 }
-
-
-/*
- X := originX + cos(angle)*radius;
- Y := originY + sin(angle)*radius;
- */
