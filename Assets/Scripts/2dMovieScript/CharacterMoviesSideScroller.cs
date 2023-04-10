104a8dc5903bb5355ce5c09b5ec7c7334986a180
WillowPunch <danek2002@gmail.com>
2023-04-10 20:05:49 +0300

    FPM
    Finished Pendulum Motion (yet bugged)

--- Assets/Scripts/2dMovieScript/CharacterMoviesSideScroller.cs
+++ Assets/Scripts/2dMovieScript/CharacterMoviesSideScroller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CharacterMoviesSideScroller : MonoBehaviour
 {
     public Rigidbody2D move;
-    public float speedCharacter = 2f;
+    public float speedCharacter = 10f;
     public float jumpForce = 10;
     public bool OnGround;
     public Transform GroundCheck;
@@ -13,30 +13,95 @@ public class CharacterMoviesSideScroller : MonoBehaviour
     public float width = 1f;
     public LayerMask Ground;
     private Vector2 vec2;
-
+    public bool Grappled = false;
+    public bool Sticky = false;
+    public bool[] Orientations = { false, false, false, false}; // [0] - up, [1] - up+direction, [2] - down, [3] - down+direction
     void Start()
     {
-       move = GetComponent<Rigidbody2D>();
+       move = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         vec2.x = Input.GetAxis("Horizontal");
-        move.velocity = new Vector2(vec2.x * speedCharacter, move.velocity.y);
-        if (OnGround)
+        if (!Sticky)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            move.velocity = new Vector2(vec2.x * speedCharacter, move.velocity.y);
+            if (move.velocity.x < 0)
             {
-                move.velocity = new Vector2(move.velocity.x, jumpForce);
+                transform.localScale = new Vector3(-1, 1, 1);
             }
-            
+            if (move.velocity.x > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            if (OnGround)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    move.velocity = new Vector2(move.velocity.x, jumpForce);
+                }
+
+            }
         }
-        ChekingGround();
-    
+        if (Grappled)
+        {
+            Sticky = true;
+        }
+        if(!Grappled && Sticky)
+        {
+            StartCoroutine(WaitForSticky());
+
+        }
+        CheckingGround();
+        OrientationCheck();
     }
 
-    void ChekingGround()
+    void CheckingGround()
     {
         OnGround = Physics2D.OverlapBox(GroundCheck.position, new Vector2(width, height), 0, Ground);
+    }
+    
+    void OrientationCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Orientations[0] = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Orientations[0] = false;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Orientations[1] = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Orientations[1] = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Orientations[2] = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Orientations[2] = false;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Orientations[3] = true;
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            Orientations[3] = false;
+        }
+    }
+
+    IEnumerator WaitForSticky()
+    {
+        yield return new WaitForSeconds(move.velocity.x * 0.05f);
+        Sticky = false;
+
     }
 }
--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -5,63 +5,161 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Grapple : MonoBehaviour
 {
-    public GameObject player;
-    public float speed = 20;
+    public float speed = 10f;
     public float height = 0.18f;
-    public float width = 1f;
+    public float width = 0.18f;
     public float timeToLive = 0;
     public LayerMask Ground;
     public DistanceJoint2D joint;
-    private bool notAttached = true;
     private Rigidbody2D rigid;
-    //private Rigidbody2D gravityPlayer;
+    private Rigidbody2D player;
+    private Vector2 playerOrient;
+    private Vector2 Orientation;
+    private bool Grounded;
+    public bool Grappled;
+    private bool[] Orientations;
+    
+
+
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        transform.position = player.transform.position;
-        rigid = gameObject.GetComponent<Rigidbody2D>();
-        //gravityPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        playerOrient = player.transform.localScale;
+        Grounded = player.GetComponent<CharacterMoviesSideScroller>().OnGround;
+        Grappled = player.GetComponent<CharacterMoviesSideScroller>().Grappled;
+        Orientations = player.GetComponent<CharacterMoviesSideScroller>().Orientations;
+        transform.position = new Vector2(player.transform.position.x, player.transform.position.y + 1f);
+        rigid = gameObject.GetComponent<Rigidbody2D>(); //getting all needed components
+        DeclareOrientation();
     }
+
+
     void Update()
     {
-        if (notAttached)
+        if (!Grappled)
         {
-            transform.Translate((speed * Time.deltaTime), (speed * Time.deltaTime), 0);
+            rigid.velocity = Orientation * speed;
         }
         if (Physics2D.OverlapBox(transform.position, new Vector2(width, height), 0, Ground))
         {
-            if (notAttached) // do not delete. to do: fix joint behaviour, works badly
+            if (!Grappled) // using the variable as a flag to execute the code just once
             {
-                
-                notAttached = false;
-                rigid.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-                joint = gameObject.AddComponent(typeof(DistanceJoint2D)) as DistanceJoint2D;
-                joint.connectedBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-                joint.anchor = transform.position;
-                joint.autoConfigureDistance = false;
-                joint.distance = 1f;
-                
+                Attacher();
             }
-           /* if(!notAttached)
-            {
-
-                
-                timeToLive += Time.deltaTime;
-                if(timeToLive > 1f)
-                {
-                    // gravityPlayer.AddForce(joint.reactionForce, ForceMode2D.Impulse);
-                    Destroy(gameObject);
-                }
-            }*/
+        }
+        if(Grappled)
+        {
+            PendulumMotion();
         }
         if (Input.GetKeyUp(KeyCode.X))
         {
-            //if(!notAttached)
-            //{ gravityPlayer.AddForce(joint.reactionForce, ForceMode2D.Impulse); }
-
+            //if(Grappled)
+            //{ 
+            //    player.AddForce(joint.reactionForce, ForceMode2D.Impulse); 
+            //}
+            Grappled = false;
+            player.GetComponent<CharacterMoviesSideScroller>().Grappled = false;
             Destroy(gameObject);
         }
     }
 
+    void Attacher()
+    {
+        Grappled = true;
+        player.GetComponent<CharacterMoviesSideScroller>().Grappled = true;
+        rigid.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+        joint = gameObject.AddComponent(typeof(DistanceJoint2D)) as DistanceJoint2D;
+        joint.connectedBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        joint.anchor = new Vector2(0, 0);
+        joint.autoConfigureDistance = false;
+        joint.connectedAnchor = new Vector2(0, 0);
+        joint.distance = 1f;
+    }
+
+    void DeclareOrientation()
+    {
+            if (Orientations[0] && (player.velocity.x == 0 || !Grounded))
+            {
+                Orientation = new Vector2(0, 1);
+            }
+            else if (Orientations[2] && !Grounded)
+            {
+                Orientation = new Vector2(0, -1);
+            }
+            else
+            {
+                if (playerOrient.x > 0)
+                {
+                    Orientation = new Vector2(1, 0);
+                }
+                else
+                {
+                    Orientation = new Vector2(-1, 0);
+                }
+                if (Orientations[1] || (Orientations[0] && player.velocity.x != 0))
+                {
+                    Orientation += new Vector2(0, 1);
+                }
+                else if (Orientations[3] || (Orientations[2] && player.velocity.x != 0))
+                {
+                    Orientation += new Vector2(0, -1);
+                }
+            }
+    }
     
+    void PendulumMotion()
+    {
+        float angle = (Mathf.Atan2(rigid.transform.position.y - player.transform.position.y, rigid.transform.position.x - player.transform.position.x) - 1.5f )/ Mathf.PI;
+        StartCoroutine(AddingForce(angle));
+        if (player.velocity.x > 35)
+        {
+            player.velocity = new Vector2(35, player.velocity.y);
+        }
+        if (player.velocity.y > 35)
+        {
+            player.velocity = new Vector2(player.velocity.x, 35);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            joint.distance = 6f;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            joint.distance = 1f;
+        }
+    }
+
+    IEnumerator AddingForce(float angle)
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (angle > 0.5f)
+                {
+                    player.AddForce(new Vector2(-1 * Math.Abs(angle) * 500, 0), ForceMode2D.Force);
+                }
+                else
+                {
+                    player.AddForce(new Vector2(Math.Abs(angle) * 500, 0), ForceMode2D.Force); //40
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (angle < -0.5f)
+                {
+                    player.AddForce(new Vector2(Math.Abs(angle) * 500, 0), ForceMode2D.Force);
+                }
+                else
+                {
+                    player.AddForce(new Vector2(-1 * Math.Abs(angle) * 500, 0), ForceMode2D.Force);
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
 }
